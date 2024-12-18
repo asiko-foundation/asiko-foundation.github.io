4e8a862cb0fa80f5f789b27ea6905ea11d29d679
YewandeOjo <yewande_oluwafisayo@outlook.com>
2024-12-19 00:26:40 +0100

    next

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 console.log("isProd", isProd);
 // const basePath = isProd ? `/${name}` : "";
 const basePath = "";
-const assetPrefix = ``;
+// const assetPrefix = ``;
+const assetPrefix = isProd ? `/${name}` : "";;
 
 module.exports = {
   reactStrictMode: true,
